60982a77cfbccc43ab060626343ef1f27bc0ac24
Jee <zeddnyx08@gmail.com>
2025-04-26 07:36:13 +0700

    feat: meta tag

---
+++ client/src/components/module/seo/index.tsx
@@ -0,0 +1,87 @@
+import { Helmet } from "react-helmet-async";
+import { config } from "@/configs";
+
+export default function Index() {
+  return (
+    <Helmet>
+      <title>Duck Council</title>
+      <meta name="description" content="A Duck that helps you debug code" />
+      <link rel="canonical" href={config.DOMAIN} />
+
+      <meta property="og:url" content={config.DOMAIN} />
+      <meta property="og:title" content="Duck Council" />
+      <meta
+        property="og:description"
+        content="A Duck that helps you debug code"
+      />
+      <meta property="og:site_name" content="Duck Council" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@site" />
+      <meta name="twitter:creator" content="@handle" />
+      <meta name="twitter:title" content="Duck Council" />
+      <meta
+        name="twitter:description"
+        content="A Duck that helps you debug code"
+      />
+
+      <script type="application/ld+json">
+        {JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "FAQPage",
+          mainEntity: [
+            {
+              "@type": "Question",
+              name: "What is the purpose of the Duck Council?",
+              acceptedAnswer: {
+                "@type": "Answer",
+                text: "To help you debug code.",
+              },
+            },
+            {
+              "@type": "Question",
+              name: "Is it safe to use the Duck Council?",
+              acceptedAnswer: {
+                "@type": "Answer",
+                text: "Yes, it is safe to use.",
+              },
+            },
+            {
+              "@type": "Question",
+              name: "What is the cost of the Duck Council?",
+              acceptedAnswer: {
+                "@type": "Answer",
+                text: "$0.00 = Free",
+              },
+            },
+          ],
+        })}
+      </script>
+
+      <script type="application/ld+json">
+        {JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          itemListElement: [
+            {
+              "@type": "ListItem",
+              position: 1,
+              name: "Duck Council",
+              item: config.DOMAIN,
+            },
+          ],
+        })}
+      </script>
+
+      <script type="application/ld+json">
+        {JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "Person",
+          name: "Jee",
+          url: "https://jaenudinv2.vercel.app",
+          sameAs: ["https://www.linkedin.com/in/jaenudin-jee-650482199"],
+        })}
+      </script>
+    </Helmet>
+  );
+}
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef, useState } from "react";
 import style from "@/styles/chat.module.css";
 import { BRAND } from "@/lib/contants";
 import { CAccordion } from "@/components/shared/accordion";
+import Seo from "@/components/module/seo";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -47,81 +48,84 @@ function Index() {
   const { isSubmit, isAnswer } = formState;
 
   return (
-    <div
-      className={cn(style.parent, "custom-scrollbar", {
-        "justify-center": !isAnswer,
-        "justify-between": isAnswer,
-      })}
-    >
-      <div className={style.content}>
-        <div className={cn(style.content_header, isAnswer && style.hide)}>
-          <h1>{BRAND.title}</h1>
-          <p>{BRAND.description}</p>
+    <>
+      <Seo />
+      <div
+        className={cn(style.parent, "custom-scrollbar", {
+          "justify-center": !isAnswer,
+          "justify-between": isAnswer,
+        })}
+      >
+        <div className={style.content}>
+          <div className={cn(style.content_header, isAnswer && style.hide)}>
+            <h1>{BRAND.title}</h1>
+            <p>{BRAND.description}</p>
+          </div>
+
+          <div className={cn(style.content_main, !isAnswer && style.hide)}>
+            {Array.from({ length: 4 }).map((_, id) => {
+              return (
+                <div
+                  key={id}
+                  className="bg-neutral-100 dark:bg-light-100 hidden md:flex w-full h-96 rounded-[12px]"
+                />
+              );
+            })}
+            <CAccordion
+              className="md:hidden"
+              defaultValue="item-1"
+              items={[
+                {
+                  value: "item-1",
+                  title: "Duck 1 (Score: 10.3)",
+                  content:
+                    "Yes. It adheres to the WAI-ARIA design pattern. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.Yes. It adheres to the WAI-ARIA design pattern. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+                },
+                {
+                  value: "item-2",
+                  title: "Is it styled?",
+                  content:
+                    "Yes. It comes with default styles that match the aesthetic.",
+                },
+                {
+                  value: "item-3",
+                  title: "Is it animated?",
+                  content:
+                    "Yes. It's animated by default, but you can disable it if you prefer.",
+                },
+              ]}
+            />
+          </div>
         </div>
 
-        <div className={cn(style.content_main, !isAnswer && style.hide)}>
-          {Array.from({ length: 4 }).map((_, id) => {
-            return (
-              <div
-                key={id}
-                className="bg-neutral-100 dark:bg-light-100 hidden md:flex w-full h-96 rounded-[12px]"
-              />
-            );
-          })}
-          <CAccordion
-            className="md:hidden"
-            defaultValue="item-1"
-            items={[
-              {
-                value: "item-1",
-                title: "Duck 1 (Score: 10.3)",
-                content:
-                  "Yes. It adheres to the WAI-ARIA design pattern. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.Yes. It adheres to the WAI-ARIA design pattern. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
-              },
-              {
-                value: "item-2",
-                title: "Is it styled?",
-                content:
-                  "Yes. It comes with default styles that match the aesthetic.",
-              },
-              {
-                value: "item-3",
-                title: "Is it animated?",
-                content:
-                  "Yes. It's animated by default, but you can disable it if you prefer.",
-              },
-            ]}
-          />
+        <div className={cn(style.content_chat, isAnswer && style.answered)}>
+          <div className={isSubmit ? style.hide : style.show}>
+            <ChatInput
+              onSend={handleSend}
+              msg={formState.situation}
+              setMsg={(value) =>
+                setFormState((prev) => ({ ...prev, situation: value }))
+              }
+              placeholder="Your Situation"
+              ref={situationRef}
+            />
+          </div>
+
+          <div className={cn(isSubmit ? [style.show] : [style.hide])}>
+            <ChatInput
+              onSend={handleSend}
+              msg={formState.action}
+              setMsg={(value) =>
+                setFormState((prev) => ({ ...prev, action: value }))
+              }
+              isBack
+              handleBack={handleBackToSituation}
+              placeholder="Your Action"
+              ref={actionRef}
+            />
+          </div>
         </div>
       </div>
-
-      <div className={cn(style.content_chat, isAnswer && style.answered)}>
-        <div className={isSubmit ? style.hide : style.show}>
-          <ChatInput
-            onSend={handleSend}
-            msg={formState.situation}
-            setMsg={(value) =>
-              setFormState((prev) => ({ ...prev, situation: value }))
-            }
-            placeholder="Your Situation"
-            ref={situationRef}
-          />
-        </div>
-
-        <div className={cn(isSubmit ? [style.show] : [style.hide])}>
-          <ChatInput
-            onSend={handleSend}
-            msg={formState.action}
-            setMsg={(value) =>
-              setFormState((prev) => ({ ...prev, action: value }))
-            }
-            isBack
-            handleBack={handleBackToSituation}
-            placeholder="Your Action"
-            ref={actionRef}
-          />
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
