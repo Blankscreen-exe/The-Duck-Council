47cdc196c19f26d8ff5e626bc2ac831bcbc06c4c
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2025-05-02 01:25:18 +0500

    add image to council response

--- duck_council/src/duck_council/api.py
+++ duck_council/src/duck_council/api.py
@@ -1,7 +1,7 @@
 from .crew import DuckCouncil
 from config import ALLOWED_AGENT_LIST
 import json
-
+from helpers import get_image_url
 
 def evaluate_action(situation: str, action: str):
     agents = ALLOWED_AGENT_LIST
@@ -11,6 +11,7 @@ def evaluate_action(situation: str, action: str):
         response = DuckCouncil().run_duck(duck_name=agent, situation=situation, action=action)
         response_dict = json.loads(str(response))
         response_dict['duck_name'] = agent
+        response_dict['image'] = get_image_url(agent)
         result.append(response_dict)
 
     return result
